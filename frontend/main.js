a20d29851c42ee5005865c78de5c1ca050a04e6a
Jamalkz1 <jamal1952@hotmail.co.uk>
2024-02-26 20:35:50 +0000

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,14 +2,14 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getresumecounterr.azurewebsites.net/api/GetResumeCounter?code=_ZVlmkO7iotALXIXJ1Rz1-jBBfIXDzmODKnxs1KNqdD3AzFuv_VHOw=='
+const functionApiUrl = 'https://getresumeecounterr.azurewebsites.net/api/GetResumeCounter?code=eIukFEfmymguRaRwL9m2b4w8wwJ2uIzPyL6zwU29nEUNAzFuf6bviA=='
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
-    }).then(response=>{
+    }).then(response =>{
         console.log ("Website called function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
